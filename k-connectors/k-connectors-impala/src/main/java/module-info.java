64dd0743ea054d767dcad9f45a00969327842e57
wangcg <1298854779@qq.com>
2023-12-27 17:46:45 +0800

    提交代码

--- k-connectors/k-connectors-impala/src/main/java/module-info.java
+++ k-connectors/k-connectors-impala/src/main/java/module-info.java
@@ -4,5 +4,7 @@ import com.ktools.warehouse.manager.datasource.KDataSourceFactory;
 module com.ktools.k.connectors.impala {
     requires com.ktools.warehouse;
 
+    opens images;
+
     provides KDataSourceFactory with ImpalaFactory;
 }
